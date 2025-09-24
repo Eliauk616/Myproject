6af1dbe3631e3a87124d989fd40d197612962d07
Eliauk616 <152280422+Eliauk616@users.noreply.github.com>
2025-09-24 19:31:52 +0800

    Delete project directory

--- project/interface.h
+++
@@ -1,8 +0,0 @@
-#ifndef __INTERFACE_H__
-#define __INTERFACE_H__
-
-void display_management_interface(unsigned int *plcd); //显示管理界面
-void display_parking_info(int num,int money, int state, int y ,int m ,int d ,int h ,int mi , unsigned int* plcd); //显示停车信息
-
-
-#endif
